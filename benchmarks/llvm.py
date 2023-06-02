98b6da7671c770d7505e8b8cd2f389799a94d0a7
rcastellotti <me@rcastellotti.dev>
2023-06-02 18:30:47 +0200

    updated llvm

--- benchmarks/llvm.py
+++ benchmarks/llvm.py
@@ -1,50 +1,65 @@
 from base import tb_benchmark_base
+import urllib.request
+import logging
+import tarfile
+import os
+import subprocess
+import time
+from tinyben import TinyBen
+from tinyben import TinyBenResult
+import shutil
+
+# https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-16.0.4.tar.gz
+# https://llvm.org/docs/GettingStarted.html#getting-the-source-code-and-building-llvm
 
 
 class tb_benchmark(tb_benchmark_base):
-    def run(self):
-        pass
+    filename = "llvmorg-16.0.4"
+    filename_tar_gz = filename + ".tar.gz"
+    pre_return_code = 1
+    cwd = ""
+    result = TinyBenResult(
+        test_fullname="llvm-project compilation (ninja)",
+        test_shortname="llvm",
+        test_status=":x:",
+        test_result=None,
+    )
 
-# import urllib.request
-# import pathlib
-# import subprocess
-# import tarfile
-# import logging
-# import subprocess
-# import os
-# import time
+    def pre(self):
+        url = (
+            "https://github.com/llvm/llvm-project/archive/refs/tags/"
+            + self.filename_tar_gz
+        )
 
-# path = pathlib.Path(__file__).parent.resolve()
-# logging.basicConfig(level=logging.DEBUG)
+        if not os.path.exists(self.filename_tar_xz):
+            logging.info(f"starting download: {url}")
+            urllib.request.urlretrieve(url, self.filename_tar_gz)
+            logging.info(f"completed download: {url}")
 
-# # https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-16.0.4.tar.gz
-# # https://llvm.org/docs/GettingStarted.html#getting-the-source-code-and-building-llvm
+        tar = tarfile.open(self.filename_tar_xz)
+        tar.extractall(path="llvm")
+        self.cwd = "llvm/" + os.listdir("llvm")[0]
 
-# llvm_tar_gz = "llvmorg-16.0.4.tar.gz"
+        self.pre_return_code = subprocess.call(
+            ["cmake", "-S llvm", "-DCMAKE_BUILD_TYPE=Release", "-B build", "-G Ninja"],
+            cwd=self.cwd,
+        )
 
-# if not os.path.exists(llvm_tar_gz):
-#     url = "https://github.com/llvm/llvm-project/archive/refs/tags/" + llvm_tar_gz
-#     logging.info(f"downloading llvm project: {url}")
-#     urllib.request.urlretrieve(url,llvm_tar_gz)
+    def run_benchmark(self):
+        if self.pre_return_code == 0:
+            start_time = time.time()
+            ret = subprocess.call(["ninja"], cwd=self.filename)
+            running_time = time.time() - start_time
+            logging.info(f"--- {running_time} seconds ---")
+            if ret == 0:
+                self.result.set_testResult(running_time)
+                self.result.set_testStatus(":white_check_mark:")
 
-# tar = tarfile.open(llvm_tar_gz)
-# if not os.path.exists("llvm-project"):
-#     tar.extractall(path="llvm-project")
+        TinyBen.results.append(self.result)
 
-# llvm_dir=os.listdir("llvm-project")[0]
-# logging.info("running command: cmake -S llvm -DCMAKE_BUILD_TYPE=Release -B build -G Ninja")
-
-# p = subprocess.call(
-#     ["cmake", "-S llvm", "-DCMAKE_BUILD_TYPE=Release", "-B build", "-G Ninja"],
-#     cwd=f"llvm-project/{llvm_dir}",
-# )
-
-# start_time = time.time()
-
-# p = subprocess.call(
-#     ["ninja"],
-#     cwd=f"llvm-project/{llvm_dir}/build",
-# )
-# logging.info("--- %s seconds ---" % (time.time() - start_time))
+    def post(self):
+        if self.pre_return_code == 0:
+            shutil.rmtree(self.cwd)
+            os.remove(self.filename_tar_gz)
 
 
