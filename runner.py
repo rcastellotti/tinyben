c007b45a4533c53c3000737683d562f591fd8bcf
rcastellotti <me@rcastellotti.dev>
2023-06-02 18:15:27 +0200

    updated runner

--- runner.py
+++ runner.py
@@ -1,29 +1,11 @@
 from tinyben import TinyBen
 import logging
-import argparse
-
-parser = argparse.ArgumentParser(prog="tinyben")
-
-
-parser.add_argument(
-    "--verbose", "-v", help="verbose", action=argparse.BooleanOptionalAction
-)
-
-args = parser.parse_args()
 
 FORMAT='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s  - %(message)s'
-
 logging.basicConfig(
     level=logging.DEBUG, format=FORMAT
 )
 
-# tb = TinyBen(benchmarks=["fake_success", "fake_failure", "llvm", "linux"])
-# tb = TinyBen(benchmarks=["fake_success", "fake_failure","imagemagick","linux"])
-# tb = TinyBen(benchmarks=["fake_success", "fake_failure","godot"])
-tb = TinyBen(benchmarks=["fake_success", "fake_failure","godot"])
-
-
+tb = TinyBen(benchmarks=["fake_success", "fake_failure","linux","imagemagick","godot"])
 tb.run()
 tb.print_results()
-
-
