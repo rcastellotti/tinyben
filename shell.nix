43a524caa8bd28f9f2215d1e3242bb0c296b98ea
rcastellotti <me@rcastellotti.dev>
2023-07-25 18:50:30 +0000

    s

--- shell.nix
+++ shell.nix
@@ -1,22 +1,50 @@
-with import <nixpkgs> { };
-mkShell {
-  nativeBuildInputs = [
-    pre-commit
-    ruff
-    git
-    python311Packages.black
-    python311Packages.requests
-    python311Packages.tqdm
-    pkg-config #redis
-    cmake #llvm
-    ninja #llvm
-    flex #linux
-    bison #linux
-    bc #linux
-    elfutils #linux
-    fakeroot #linux
-    openssl #linux
-    scons #godot
-    lz4 #lz4 :)
-  ];
-}   
+# let
+#   pkgs = import <nixpkgs> {};
+# in
+# pkgs.mkShell {
+#   nativeBuildInputs = with pkgs; [
+#     pkgs.pre-commit
+#     ruff
+#     git
+#     pkg-config #redis
+#     cmake #llvm
+#     ninja #llvm
+#     flex #linux
+#     bison #linux
+#     bc #linux
+#     elfutils #linux
+#     fakeroot #linux
+#     openssl #linux
+#     scons #godot
+#     lz4 #lz4 :)
+#     python3.withPackages (pyPkgs: with pyPkgs; [ black requests tqdm])
+
+#   ];
+# }   
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (ps: [
+      ps.black
+      ps.requests
+      ps.tqdm
+    ]))
+
+  pkgs.pre-commit
+  pkgs.ruff
+  pkgs.git
+  pkgs.pkg-config # redis
+  pkgs.cmake # llvm
+  pkgs.ninja # llvm
+  pkgs.flex # linux
+  pkgs.bison # linux
+  pkgs.bc # linux
+  pkgs.elfutils # linux
+  pkgs.fakeroot # linux
+  pkgs.openssl # linux
+  pkgs.scons # godot
+  pkgs.lz4 # lz4 :)
+];
+}
--- tinyben/benchmarks/lz4.py
+++ tinyben/benchmarks/lz4.py
@@ -8,31 +8,33 @@ from datetime import datetime
 
 
 def main():
+    cache = os.path.join(os.getcwd(), ".cache")
     timestamp = datetime.now()
     common.add_header_to_file("lz4", ["timestamp", "type", "completion_time_ms"])
-    os.makedirs(".cache", exist_ok=True)
+    cwd = os.path.join(cache, "lz4")
 
-    if not os.path.exists(".cache/lz4"):
+    if not os.path.exists(cwd):
+        os.makedirs(cwd, exist_ok=True)
         common.download_file(
             "https://releases.ubuntu.com/jammy/ubuntu-22.04.2-desktop-amd64.iso",
-            ".cache/ubuntu-22.04.2-desktop-amd64.iso",
+            os.path.join(cwd, "ubuntu-22.04.2-desktop-amd64.iso"),
             skip_if_exists=True,
         )
 
     start_time = time.time()
-    subprocess.call(["lz4", "ubuntu-22.04.2-desktop-amd64.iso"], cwd=".cache/lz4")
+    subprocess.call(["lz4", "ubuntu-22.04.2-desktop-amd64.iso"], cwd=cwd)
     completion_time_ms = (time.time() - start_time) * 1000
     common.add_to_result_file("lz4", [timestamp, "compression", completion_time_ms])
 
     start_time = time.time()
     subprocess.call(
         ["lz4", "ubuntu-22.04.2-desktop-amd64.iso.lz4", "decompressed.iso"],
-        cwd=".cache/lz4",
+        cwd=cwd,
     )
     completion_time_ms = (time.time() - start_time) * 1000
     common.add_to_result_file("lz4", [timestamp, "decompression", completion_time_ms])
 
-    shutil.rmtree(".cache/lz4")
+    shutil.rmtree(cwd)
 
 
 if __name__ == "__main__":
